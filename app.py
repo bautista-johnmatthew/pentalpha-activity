7aa0bf7e180d33e8d2b0a2acb2201b6b494dcf41
Precious Hannah Corpus <corpus.precioushannah1@gmail.com>
2025-07-05 15:03:11 +0800

    Add Corpus-3

---
+++ app.py
@@ -0,0 +1,13 @@
+# CORPUS: 3 points
+
+from flask import Flask, request, jsonify
+from models import view_notes
+
+app = Flask(__name__)
+
+@app.route('/notes', methods=['GET'])
+def get_note():
+    return jsonify(view_notes())
+
+app.run()
+
